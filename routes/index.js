1f492f615c4a3b85164312dcd6c988a88a4de717
Julien Sarazin <julien.sarazin@me.com>
2018-01-09 14:56:04 +0100

    updated idylle dependency

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser');
 
 module.exports = (app) => {
-    app.server.use(bodyParser.raw({ type: '*/*', limit: app.settings.body_limit }), app.resolver.proxy());
+    app.router.use(bodyParser.raw({ type: '*/*', limit: app.settings.body_limit }), app.resolver.proxy());
 };
 
